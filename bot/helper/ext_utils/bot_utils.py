d34a6c48e025f7d59cbc0da2ea361f8db89a710d
Esmaeil <77193962+esmail-mkh@users.noreply.github.com>
2022-06-08 15:42:14 +0430

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -122,7 +122,7 @@ def get_readable_message():
                 globals()['COUNT'] -= STATUS_LIMIT
                 globals()['PAGE_NO'] -= 1
         for index, download in enumerate(list(download_dict.values())[COUNT:], start=1):
-            msg += f"<b>⋖ SoulLand Leecher ⋗</b>
+            msg += f"<b>⋖ SoulLand Leecher ⋗</b>"
             msg += f"<b>┏ Name:</b> <code>{escape(str(download.name()))}</code>"
             msg += f"\n<b>┣ Status:</b> <i>{download.status()}</i>"
             if download.status() not in [
