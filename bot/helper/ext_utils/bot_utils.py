ed84788b2e25e21307cd78160c731f2c493c8a44
Esmaeil <77193962+esmail-mkh@users.noreply.github.com>
2022-06-08 17:48:42 +0430

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -122,9 +122,9 @@ def get_readable_message():
                 globals()['COUNT'] -= STATUS_LIMIT
                 globals()['PAGE_NO'] -= 1
         for index, download in enumerate(list(download_dict.values())[COUNT:], start=1):
-            msg += f"<b>⋖ SoulLand Leecher ⋗</b>"
-            msg += f"<b>┏ Name:</b> <code>{escape(str(download.name()))}</code>"
-            msg += f"\n<b>┣ Status:</b> <i>{download.status()}</i>"
+            msg += f"<b>⋖ SσυʅLαɳԃ Lҽҽƈԋҽɾ ⋗</b>"
+            msg += f"<b>┏ ⊹𝐍𝐚𝐦𝐞:</b> <code>{escape(str(download.name()))}</code>"
+            msg += f"\n<b>┣ ⊹𝐒𝐭𝐚𝐭𝐮𝐬:</b> <i>{download.status()}</i>"
             if download.status() not in [
                 MirrorStatus.STATUS_ARCHIVING,
                 MirrorStatus.STATUS_EXTRACTING,
@@ -133,37 +133,37 @@ def get_readable_message():
             ]:
                 msg += f"\n{get_progress_bar_string(download)} {download.progress()}"
                 if download.status() == MirrorStatus.STATUS_CLONING:
-                    msg += f"\n<b>┣ Cloned:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
+                    msg += f"\n<b>┣ ⊹𝐂𝐥𝐨𝐧𝐞𝐝:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
                 elif download.status() == MirrorStatus.STATUS_UPLOADING:
-                    msg += f"\n<b>┣ Uploaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
+                    msg += f"\n<b>┣ ⊹𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐝:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
                 else:
-                    msg += f"\n<b>┣ Downloaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
-                msg += f"\n<b>┣ Speed:</b> {download.speed()} | <b>ETA:</b> {download.eta()}"
+                    msg += f"\n<b>┣ ⊹𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝𝐞𝐝:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
+                msg += f"\n<b>┣ ⊹𝐒𝐩𝐞𝐞𝐝:</b> {download.speed()} | <b>⊹𝐄𝐓𝐀:</b> {download.eta()}"
                 try:
-                    msg += f"\n<b>┣ Seeders:</b> {download.aria_download().num_seeders}" \
-                           f" | <b>Peers:</b> {download.aria_download().connections}"
+                    msg += f"\n<b>┣ ⊹𝐒𝐞𝐞𝐝𝐞𝐫𝐬:</b> {download.aria_download().num_seeders}" \
+                           f" | <b>⊹𝐏𝐞𝐞𝐫𝐬:</b> {download.aria_download().connections}"
                 except:
                     pass
                 try:
-                    msg += f"\n<b>┣ Seeders:</b> {download.torrent_info().num_seeds}" \
-                           f" | <b>Leechers:</b> {download.torrent_info().num_leechs}"
+                    msg += f"\n<b>┣ ⊹𝐒𝐞𝐞𝐝𝐞𝐫𝐬:</b> {download.torrent_info().num_seeds}" \
+                           f" | <b>⊹𝐋𝐞𝐞𝐜𝐡𝐞𝐫𝐬:</b> {download.torrent_info().num_leechs}"
                 except:
                     pass
                 msg += f"\n<code>┗ /{BotCommands.CancelMirror} {download.gid()}</code>"
             elif download.status() == MirrorStatus.STATUS_SEEDING:
-                msg += f"\n<b>┣ Size: </b>{download.size()}"
-                msg += f"\n<b>┣ Speed: </b>{get_readable_file_size(download.torrent_info().upspeed)}/s"
-                msg += f" | <b>┣ Uploaded: </b>{get_readable_file_size(download.torrent_info().uploaded)}"
-                msg += f"\n<b>┣ Ratio: </b>{round(download.torrent_info().ratio, 3)}"
-                msg += f" | <b>┣ Time: </b>{get_readable_time(download.torrent_info().seeding_time)}"
+                msg += f"\n<b>┣ ⊹𝐒𝐢𝐳𝐞: </b>{download.size()}"
+                msg += f"\n<b>┣ ⊹𝐒𝐩𝐞𝐞𝐝: </b>{get_readable_file_size(download.torrent_info().upspeed)}/s"
+                msg += f" | <b>┣ ⊹𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐝: </b>{get_readable_file_size(download.torrent_info().uploaded)}"
+                msg += f"\n<b>┣ ⊹𝐑𝐚𝐭𝐢𝐨: </b>{round(download.torrent_info().ratio, 3)}"
+                msg += f" | <b>┣ ⊹𝐓𝐢𝐦𝐞: </b>{get_readable_time(download.torrent_info().seeding_time)}"
                 msg += f"\n<code>┗ /{BotCommands.CancelMirror} {download.gid()}</code>"
             else:
-                msg += f"\n<b>┣ Size: </b>{download.size()}"
+                msg += f"\n<b>┣ ⊹𝐒𝐢𝐳𝐞: </b>{download.size()}"
             msg += "\n\n"
             if STATUS_LIMIT is not None and index == STATUS_LIMIT:
                 break
-        bmsg = f"<b>┏ CPU:</b> {cpu_percent()}% | <b>FREE:</b> {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)}"
-        bmsg += f"\n<b>┣ RAM:</b> {virtual_memory().percent}% | <b>UPTIME:</b> {get_readable_time(time() - botStartTime)}"
+        bmsg = f"<b>┏ ▫️𝐂𝐏𝐔:</b> {cpu_percent()}% | <b>▫️𝐅𝐑𝐄𝐄:</b> {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)}"
+        bmsg += f"\n<b>┣ ▪️𝐑𝐀𝐌:</b> {virtual_memory().percent}% | <b>⏱️𝐔𝐏𝐓𝐈𝐌𝐄:</b> {get_readable_time(time() - botStartTime)}"
         dlspeed_bytes = 0
         upspeed_bytes = 0
         for download in list(download_dict.values()):
@@ -178,12 +178,12 @@ def get_readable_message():
                     upspeed_bytes += float(spd.split('K')[0]) * 1024
                 elif 'MB/s' in spd:
                     upspeed_bytes += float(spd.split('M')[0]) * 1048576
-        bmsg += f"\n<b>┗ DL:</b> {get_readable_file_size(dlspeed_bytes)}/s | <b>UL:</b> {get_readable_file_size(upspeed_bytes)}/s"
+        bmsg += f"\n<b>┗ 📥𝐃𝐋:</b> {get_readable_file_size(dlspeed_bytes)}/s | <b>📤𝐔𝐋:</b> {get_readable_file_size(upspeed_bytes)}/s"
         if STATUS_LIMIT is not None and tasks > STATUS_LIMIT:
-            msg += f"<b>🔹Page:</b> {PAGE_NO}/{pages} | <b>🔸Tasks:</b> {tasks}\n"
+            msg += f"<b>🔹𝐏𝐚𝐠𝐞:</b> {PAGE_NO}/{pages} | <b>🔸𝐓𝐚𝐬𝐤𝐬:</b> {tasks}\n"
             buttons = ButtonMaker()
             buttons.sbutton("⬅️Previous", "status pre")
-            buttons.sbutton("➡️Next", "status nex")
+            buttons.sbutton("Next➡️", "status nex")
             button = InlineKeyboardMarkup(buttons.build_menu(2))
             return msg + bmsg, button
         return msg + bmsg, ""
